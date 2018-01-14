ec9c7bb741ef947bb541b0a4ce6909d0caa804d3
Andrzej <zapala333@gmail.com>
2018-01-14 18:00:37 +0100

    Sufficient to pass

--- src/julia.s
+++ src/julia.s
@@ -2,15 +2,24 @@
 
     section .text
     global julia
+
+;rdi - pixelBuffer
+;rsi -width
+;rdx - high
+;xmm0 - zoom
+;xmm1 - x0
+;xmm2 - y0
+;xmm3 - real of Complex
+;xmm4 - imaginary of Complex
+
+
 julia:
     push rbp
     mov rbp,rsp
     mov r8,0
     mov r9,0
-    mov r10,0x000000
-    mov r11,0xffffff
-    movsd xmm13,xmm0 ; actual x
-    movsd xmm14,xmm1 ; actual y
+    movsd xmm13,xmm0 ; curent x
+    movsd xmm14,xmm1 ; curent y
 
 
 loopPixel:
@@ -18,58 +27,53 @@ loopPixel:
    je nextLine
    cmp r9,rdx
    je end
-   mov rax,0 ; iteration
-   mov rcx,100; max of iteration
+   mov rax,0x000000 ; iteration
+   mov rcx,0xffffff; max of iteration
    movsd xmm8,xmm13
    movsd xmm9,xmm14
 
 loopJulia:
-   movsd xmm10,xmm8; actual x
-   mulsd xmm10,xmm10 ; x^2
+   movsd xmm10,xmm8; curent xz
+   mulsd xmm10,xmm10 ; xz^2
 
-   movsd xmm11,xmm9; actual y
-   mulsd xmm11,xmm11; y^2
+   movsd xmm11,xmm9; curent yz
+   mulsd xmm11,xmm11; yz^2
    movsd xmm12,xmm10
-   addsd xmm12,xmm11 ; x^2 + y^2
+   addsd xmm12,xmm11 ; xz^2 + yz^2
    ucomisd xmm12,xmm5 ;xmm5 is 4
-   ja colourWhitePixel
-   jp colourWhitePixel
+   ja next
+   jp next
 
 
    movsd xmm12, xmm10
-   subsd xmm12,xmm11 ; x^2-y^2
+   subsd xmm12,xmm11 ; xz^2-yz^2
 
-   addsd xmm9,xmm9 ;2*y
-   mulsd xmm9,xmm8 ;2*x*y
-   addsd xmm9,xmm4;2*x*y+cy
+   addsd xmm9,xmm9 ;2*yz
+   mulsd xmm9,xmm8 ;2*xz*yz
+   addsd xmm9,xmm4;2*xz*yz+cy
 
    movsd xmm8,xmm12
-   addsd xmm8,xmm3;x^2-y^2+cx
+   addsd xmm8,xmm3;xz^2-yz^2+cx
 
-   add rax,1
+   add rax,0x010101
    cmp rax,rcx
-   jge colourBlackPixel
+   jge next
 
    jmp loopJulia
 
-colourWhitePixel:
-   mov [rdi],r10
-   jmp next
-
-colourBlackPixel:
-    mov[rdi],r11
 
 next:
+   mov [rdi],rax
    inc r8
    add rdi,3
-   addsd xmm13,xmm2
+   addsd xmm13,xmm2 ; actual x + zoom
    jmp loopPixel
 
 nextLine:
     mov r8,0
     inc r9
-    movsd xmm13,xmm0
-    addsd xmm14,xmm2
+    movsd xmm13,xmm0 ; acual x = x0
+    addsd xmm14,xmm2    ;actual x + zoom
     jmp loopPixel
 
 end:
--- src/main.c
+++ src/main.c
@@ -4,30 +4,45 @@
 #include <stdlib.h>
 
 
-const int SCREEN_W =1000;
-const int SCREEN_H =1000;
+const int SCREEN_W =2000;
+const int SCREEN_H =2000;
 
-void modiifyBitMap(ALLEGRO_BITMAP *bmp)
+
+
+void modiifyBitMap(ALLEGRO_BITMAP *bmp, double x0,double y0,double zoom, double real,double imaginary)
 {
     ALLEGRO_LOCKED_REGION *lr = al_lock_bitmap(bmp, ALLEGRO_PIXEL_FORMAT_RGB_888, ALLEGRO_LOCK_READWRITE);
     unsigned char*ptr = (unsigned char*) lr->data; //points to first pixel
 
-    ptr-=SCREEN_W*(SCREEN_H-1)*3;
-    double czas=al_get_time();
-    printf("%lf\n",czas);
-    julia(ptr,SCREEN_W,SCREEN_H,-1.5,-1.5,0.003,-0.4,0.6,4.0);
-    czas=al_get_time();
-    printf("%lf\n",czas);
+    ptr-=SCREEN_W*(SCREEN_H-1)*3; //it's get the last element
+
+    julia(ptr,SCREEN_W,SCREEN_H,x0,y0,zoom,real,imaginary,4.0);
 
     al_unlock_bitmap(bmp);
+
+
     al_clear_to_color(al_map_rgb(0, 0, 0));
     al_draw_bitmap(bmp,0,0,0);
 
     al_flip_display();
 }
 
+void changeIncrease(double *x0,double *y0,double *zoom,double clickX, double clickY,double howMuch)
+{
+
+
+    *x0+=clickX*(*zoom);
+    *y0+=(SCREEN_H-clickY)*(*zoom);
+    *zoom*=howMuch;
+
+    *x0-=SCREEN_W*(*zoom)*0.5;
+    *y0-=SCREEN_H*(*zoom)*0.5;
+
+}
+
 int main(int argc, char **argv)
 {
+
     ALLEGRO_DISPLAY *display = NULL;
     ALLEGRO_EVENT_QUEUE *event_queue = NULL;
     ALLEGRO_BITMAP *bmp = NULL;
@@ -44,6 +59,11 @@ int main(int argc, char **argv)
         return -1;
     }
 
+    if(!al_install_keyboard())
+    {
+        fprintf(stderr, "failed to initialize the keyboard!\n");
+        return 1;
+    }
 
     display = al_create_display(SCREEN_W, SCREEN_H);
 
@@ -69,6 +89,7 @@ int main(int argc, char **argv)
     al_flip_display();
 
     event_queue = al_create_event_queue();
+
     if (!event_queue)
     {
         fprintf(stderr, "failed to create event_queue!\n");
@@ -78,10 +99,17 @@ int main(int argc, char **argv)
 
 
     al_register_event_source(event_queue, al_get_display_event_source(display));
-
     al_register_event_source(event_queue, al_get_mouse_event_source());
-
+    al_register_event_source(event_queue, al_get_keyboard_event_source());
     al_set_target_bitmap(al_get_backbuffer(display));
+
+    double x0=-2.0;
+    double y0=-2.0;
+    double real=-0.4;
+    double zoom=(-2.0)*x0/SCREEN_W;
+    double imaginary=0.6;
+
+    modiifyBitMap(bmp,x0,y0,zoom,real,imaginary);
 
     while (1)
     {
@@ -94,7 +122,37 @@ int main(int argc, char **argv)
         }
         else if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN)
         {
-            modiifyBitMap(bmp);
+            double clickX = ev.mouse.x;
+            double clickY = ev.mouse.y;
+
+            if(ev.mouse.button==1) //leftclick
+            {
+                changeIncrease(&x0,&y0,&zoom,clickX,clickY,0.5);
+            }
+            else if(ev.mouse.button==2)
+            {
+                changeIncrease(&x0,&y0,&zoom,clickX,clickY,2);
+            }
+
+            modiifyBitMap(bmp, x0, y0, zoom, real, imaginary);
+        }
+        else if (ev.type == ALLEGRO_EVENT_KEY_DOWN && ev.keyboard.keycode ==ALLEGRO_KEY_R)
+        {
+            printf("Write real of complex\n");
+            scanf("%lf",&real);
+            x0=-2.0;
+            y0=-2.0;
+            zoom=(-2.0)*x0/SCREEN_W;
+            modiifyBitMap(bmp,x0,y0,zoom,real,imaginary);
+        }
+        else if (ev.type == ALLEGRO_EVENT_KEY_DOWN && ev.keyboard.keycode ==ALLEGRO_KEY_I)
+        {
+            printf("Write imaginary of complex\n");
+            scanf("%lf",&imaginary);
+            x0=-2.0;
+            y0=-2.0;
+            zoom=(-2.0)*x0/SCREEN_W;
+            modiifyBitMap(bmp,x0,y0,zoom,real,imaginary);
         }
 
     }
